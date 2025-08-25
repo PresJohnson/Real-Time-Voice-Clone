41bcc3d7926967cc414d06d957cf21013eb07253
PresJohnson <pres.johnson19@gmail.com>
2025-08-25 05:57:54 -0700

    Update Some lines

--- encoder_train.py
+++ encoder_train.py
@@ -17,6 +17,10 @@ if __name__ == "__main__":
     parser.add_argument("clean_data_root", type=Path, help= \
         "Path to the output directory of encoder_preprocess.py. If you left the default "
         "output directory when preprocessing, it should be <datasets_root>/SV2TTS/encoder/.")
+    parser.add_argument("-m", "--models_dir", type=Path, default="saved_models", help=\
+        "Path to the root directory that contains all models. A directory <run_name> will be created under this root."
+        "It will contain the saved model weights, as well as backups of those weights and plots generated during "
+        "training.")
     parser.add_argument("-v", "--vis_every", type=int, default=10, help= \
         "Number of steps between updates of the loss and the plots.")
     parser.add_argument("-u", "--umap_every", type=int, default=100, help= \
